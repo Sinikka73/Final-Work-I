32f557004a62cd9cb8215c41f7a20b678b71d217
olga <bend_sinister@mail.ru>
2022-11-29 14:09:34 +0300

    добавление кода

--- Program.cs
+++ Program.cs
@@ -3,3 +3,61 @@
 // равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма. При решении 
 //не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] generateArray(int length)
+{
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i + 1}-й элемент массива:");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+void printArray(string[] arrayToPrint, string name)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"------------{name} -----------");
+    Console.ResetColor();
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write($"\"{arrayToPrint[i]}\"");      
+        if(i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
+
+string[] GetResultArray(string[] array)
+{		
+	string[] result = new string[array.Length];
+	int count = 0;	
+	for (int i = 0; i < array.Length; i++)
+	{
+		if(array[i].Length <= 3)
+		{
+			result[count] = array[i];
+			count++;
+		}
+	}
+	Array.Resize(ref result, count);
+	return result;
+}
+
+Console.WriteLine();
+Console.Write("Введите количество элементов в массиве: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+if(lenght > 0)
+{
+    Console.WriteLine();
+    string[] currentArray = generateArray(lenght);
+    printArray(currentArray, "Исходный массив");
+    string[] finalArray = GetResultArray(currentArray);
+    printArray(finalArray , "Полученный массив");
+}
+else Console.WriteLine("Задача не имеет решения");
